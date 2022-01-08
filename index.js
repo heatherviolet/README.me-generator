21066c69d9e7c5594dddd4a8db72dfba269085dc
Heather Violet <heather@heathers-air.lan>
2022-01-07 21:21:17 -0800

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+console.log(inquirer);
 // TODO: Create an array of questions for user input
 const questions = [];
 

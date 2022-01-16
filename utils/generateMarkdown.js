60b3b019a003fb3201135d371197c62c8cce88cd
Heather Violet <heather@heathers-air.lan>
2022-01-16 15:42:24 -0800

    update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -41,8 +41,7 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${renderLicenseLink(data.license)}
-  ${renderLicenseBadge(data.license)}
+  ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
 
   ## Description
   ${data.description}
